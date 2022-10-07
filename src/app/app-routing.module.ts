5b285c77a20fcda2c5bc5d22870f1e1f6a7aed01
Daro-Sim <daroandrot@gmail.com>
2022-10-07 09:34:18 +0700

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,32 +4,42 @@ import { ForgotPasswordComponent } from './authentication/forgot-password/forgot
 import { LoginComponent } from './authentication/login/login.component';
 import { RegisterComponent } from './authentication/register/register.component';
 import { VerifyEmailComponent } from './authentication/verify-email/verify-email.component';
+import { AdminDashboardComponent } from './product/admin-dashboard/admin-dashboard.component';
 import { ViewAllProductComponent } from './product/view-all-product/view-all-product.component';
 import { ViewItemInCartComponent } from './product/view-item-in-cart/view-item-in-cart.component';
 import { ViewProductLoggedInComponent } from './product/view-product-logged-in/view-product-logged-in.component';
 import { ViewProductComponent } from './product/view-product/view-product.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'view-all-product', pathMatch: 'full'},
-  {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent},
-  {path: 'verify-email', component: VerifyEmailComponent},
-  {path: 'forgot-password', component: ForgotPasswordComponent},
-  {path: 'view-all-product', component: ViewAllProductComponent},
-  {path: 'view-product/:id', component: ViewProductComponent},
-  {path: 'view-all-product/view-product/:id', component: ViewProductComponent},
-  {path: 'view-all-product/login', component: LoginComponent},
-  {path: 'view-product/:id/login', component: LoginComponent},
-  {path: 'view-all-product/view-product/:id/login', component: LoginComponent},
-  {path: 'view-all-product/view-product/:id/cart', component: ViewItemInCartComponent},
-  {path: 'view-all-product/cart', component: ViewItemInCartComponent},
-  {path: 'view-product-logged-in', component: ViewProductLoggedInComponent},
-  {path: 'view-product-logged-in/cart', component: ViewItemInCartComponent},
-
+  { path: '', redirectTo: 'view-all-product', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'verify-email', component: VerifyEmailComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'view-all-product', component: ViewAllProductComponent },
+  { path: 'view-product/:id', component: ViewProductComponent },
+  {
+    path: 'view-all-product/view-product/:id',
+    component: ViewProductComponent,
+  },
+  { path: 'view-all-product/login', component: LoginComponent },
+  { path: 'view-product/:id/login', component: LoginComponent },
+  {
+    path: 'view-all-product/view-product/:id/login',
+    component: LoginComponent,
+  },
+  {
+    path: 'view-all-product/view-product/:id/cart',
+    component: ViewItemInCartComponent,
+  },
+  { path: 'view-all-product/cart', component: ViewItemInCartComponent },
+  { path: 'view-product-logged-in', component: ViewProductLoggedInComponent },
+  { path: 'view-product-logged-in/cart', component: ViewItemInCartComponent },
+  { path: 'admin-dashboard', component: AdminDashboardComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
